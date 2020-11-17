ed7c13b081f01dbebf59c65e87cddcbfff0496a1
罗利华 <luolihua@beadwallet.com>
2020-11-17 17:22:17 +0800

    format code

--- rabbitmq01/src/main/java/com/vic/App.java
+++ rabbitmq01/src/main/java/com/vic/App.java
@@ -2,12 +2,10 @@ package com.vic;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Long a = 34324L;
+        System.out.println(a + "1_sa.jpg");
     }
 }
--- rabbitmq02/src/main/java/com/vic/App.java
+++ rabbitmq02/src/main/java/com/vic/App.java
@@ -2,12 +2,9 @@ package com.vic;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
